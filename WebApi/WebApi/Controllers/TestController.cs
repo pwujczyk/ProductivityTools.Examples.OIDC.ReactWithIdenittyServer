97108864e044065a4704bbd5c02dcd9b8341983b
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-01 23:18:49 +0200

    react project

---
+++ WebApi/WebApi/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("Hi")]
+        public string Hi()
+        {
+            return $"Hi {DateTime.Now}";
+        }
+
+        [HttpGet]
+        [Route("HiSecure")]
+        public string HiSecure()
+        {
+            return $"Hi secure {DateTime.Now}";
+        }
+    }
+}
