d5558f9e67ca661d31094ab39f5cc6965d462634
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-02 07:02:29 +0200

    call endpint

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [setDate] = useState([]);
+
+  useEffect(()=>
+  {
+    fetch(`https://localhost:44307/Test/Hi`, {method:"GET"})
+    .then(res=>res.json())
+    .then(res=>setDate(res));
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Endpoint not secured:</p>
     </div>
   );
 }
