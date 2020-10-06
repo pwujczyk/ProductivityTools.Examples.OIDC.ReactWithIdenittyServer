17ce67983ff8d526e09e9d08831648cc0d3c3994
Pawel Wujczyk <pwujczyk@ecovadis.com>
2020-10-06 22:18:06 +0200

    Automatic Commit

--- reactapp/src/utils/privateroute.js
+++ reactapp/src/utils/privateroute.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-
-
 function PrivateRoute({ children, component: Component, ...rest }) {
     const user = useSelector(state => state.auth.user)
     return user
